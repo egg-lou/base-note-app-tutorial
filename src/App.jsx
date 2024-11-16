93dd757a4a4297e3a5dfd984e1a04bf6ae90f599
Rafael Louie Miguel <118577897+egg-lou@users.noreply.github.com>
2024-11-16 12:45:20 +0800

    Added lucidre react and slight styling changes

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,7 @@ function App() {
   };
 
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 py-8 flex flex-col">
       <h1 className="text-4xl font-bold text-center mb-8 font-handwriting">
         Scribbly
       </h1>
